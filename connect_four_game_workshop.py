4743e0e9cabb31638abc8abfe2ea3f7d6f56c05b
Georgi St. Lukanov <102332504+GeorgiLukanov87@users.noreply.github.com>
2022-10-19 13:23:17 +0200

    Update connect_four_game_workshop.py

--- connect_four_game_workshop.py
+++ connect_four_game_workshop.py
@@ -1,3 +1,7 @@
+# Just Run the code and enjoy :) !
+# The default board game SIZE is 6x7.
+# You can easily change the board-size on line = 106 and 107 by changing -> "rows_count" and "cows_count" with the size you wish!
+
 from collections import deque
 
 
